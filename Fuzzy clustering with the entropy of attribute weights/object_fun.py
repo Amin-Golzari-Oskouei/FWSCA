cf6214d0b0bdaf7ce9379791b49d5fb9bc4417fb
Amin Golzari Oskouei <51666357+Amin-Golzari-Oskouei@users.noreply.github.com>
2024-02-14 20:19:24 +0330

    object_fun.py

--- Fuzzy clustering with the entropy of attribute weights/object_fun.py
+++ Fuzzy clustering with the entropy of attribute weights/object_fun.py
@@ -12,6 +12,6 @@ def object_fun(row, col, k, Cluster_elem, center_points, fuzzy_degree, data, lan
     WBETA = np.transpose(z[j, :])
     WBETA[np.where(np.isinf(WBETA))] = 0 
     j_fun1 = np.sum(np.sum(dNK * np.transpose(Cluster_elem ** fuzzy_degree)))
-    j_fun2 = (1/landa) * np.sum(np.sum(np.matmul(np.expand_dims(WBETA,1) , np.transpose(np.log(np.expand_dims(WBETA,1))))))
+    j_fun2 = (1/landa) * np.sum(np.sum(np.matmul(np.expand_dims(WBETA,1) , np.transpose(np.log2(np.expand_dims(WBETA,1))))))
     j_fun  = j_fun1 + j_fun2
     return j_fun
