dc7facbff3a6359714b60e9f486c75470adc629f
Amin Golzari Oskouei <51666357+Amin-Golzari-Oskouei@users.noreply.github.com>
2024-03-09 20:27:16 +0330

    Add files via upload

---
+++ Unsupervised learning of prototypes and attribute weights -SCAD2/Unsupervised learning of prototypes and attribute weights _ SCAD2/main.py
@@ -0,0 +1,105 @@
+def main(data, center_points, k, t_max, row, fuzzy_degree, col, q):
+    import numpy as np
+    import math
+    import sys
+    from object_fun import object_fun
+    
+    if q <= 1:
+        print('Error: q must be a more than 1', file=sys.stderr)
+        sys.exit()
+
+   
+#-----------------------------------------------------------------------------------------
+    # Other initializations.
+    Iter = 1                  # Number of iterations.
+    E_w_old = math.inf        # Previous iteration objective (used to check convergence).
+
+
+    # Weights are uniformly initialized.
+    z = np.ones([k, col]) / col
+
+    # Other initializations.
+    Cluster_elem = np.zeros([k, row])
+
+    distance = np.zeros([k, row, col])
+    dNK = np.zeros([row, k])
+    dwkm= np.zeros([k, col])
+    # --------------------------------------------------------------------------
+    
+    print('Start of Fuzzy C-Means iterations')
+    print('----------------------------------')
+
+    # the algorithm iteration procedure
+    while 1:
+
+        # Update the cluster assignments.
+        for j in range(k):
+            distance[j, :, :] = (data - np.tile(center_points[j, :], (row, 1))) ** 2
+            WBETA = np.transpose(z[j, :] ** q)
+            WBETA[np.where(np.isinf(WBETA))] = 0
+            dNK[:, j] = np.squeeze(np.matmul(np.reshape(distance[j, :, :], (row, col)), np.expand_dims(WBETA, 1)))
+
+        tmp1 = np.zeros([row, k])
+        for j in range(k):
+            tmp2 = (dNK / np.transpose(np.tile(dNK[:, j], (k, 1)))) ** (1 / (fuzzy_degree - 1))
+            tmp2[np.where(np.isnan(tmp2))] = 0
+            tmp2[np.where(np.isinf(tmp2))] = 0
+            tmp1 = tmp1 + tmp2
+
+        Cluster_elem = np.transpose(1 / tmp1)
+        Cluster_elem[np.where(np.isnan(Cluster_elem))] = 1
+        Cluster_elem[np.where(np.isinf(Cluster_elem))] = 1
+
+        for j in np.where(dNK == 0)[0]:
+            Cluster_elem[np.where(dNK[j, :] == 0)[0], j] = 1 / len(np.where(dNK[j, :] == 0)[0])
+            Cluster_elem[np.where(dNK[j, :] != 0)[0], j] = 0
+
+
+        # Calculate the MinMax k-means objective.
+        E_w = object_fun(row, col, k, Cluster_elem, center_points, fuzzy_degree, z, q, data)
+
+        if math.isnan(E_w) == False:
+            print(f'The algorithm objective is E_w={E_w}')
+
+        # Check for convergence. Never converge if in the current (or previous)
+        # iteration empty or singleton clusters were detected.
+        if (math.isnan(E_w) == False) and (math.isnan(E_w_old) == False) and (abs(1 - E_w / E_w_old) < 10**-6 or Iter >= t_max):
+
+            print('++++++++++++++++++++++++++++++++++++++++++++++++++++++++')
+            print(f'Converging for after {Iter} iterations.')
+            print(f'The proposed algorithm objective is E_w={E_w}.')
+
+            return Cluster_elem
+
+        E_w_old = E_w
+
+        # Update the cluster centers.
+
+        mf = Cluster_elem ** fuzzy_degree
+        for j in range(k):
+            center_points[j, :] = np.matmul(np.expand_dims(mf[j, :], 0), data) / np.matmul(np.expand_dims(mf[j, :], 0), np.ones_like(data))
+
+        
+        # # Update the feature weights.
+        for j in range(k):
+            distance[j, :, :] = (data - np.tile(center_points[j, :], (row, 1))) ** 2
+            dwkm[j, :] = np.matmul(mf[j, :], np.reshape(distance[j, :, :], (row, col)))
+            
+
+        tmp1 = np.zeros([k, col])
+        for j in range(col):
+            tmp2 = (dwkm / (np.tile(np.expand_dims(dwkm[:, j],1), (1, col)))) ** (1 / (q - 1))
+            tmp2[np.where(np.isnan(tmp2))] = 0
+            tmp2[np.where(np.isinf(tmp2))] = 0
+            tmp1 = tmp1 + tmp2
+
+        z = (1 / tmp1)
+        z[np.where(np.isnan(z))] = 1
+        z[np.where(np.isinf(z))] = 1
+
+        for j in np.where(dwkm == 0)[0]:
+            z[j, np.where(dwkm[j, :] == 0)[0]] = 1 / len(np.where(dwkm[j, :] == 0)[0])
+            z[j, np.where(dwkm[j, :] != 0)[0]] = 0
+
+
+        Iter = Iter + 1
---
+++ Unsupervised learning of prototypes and attribute weights -SCAD2/Unsupervised learning of prototypes and attribute weights _ SCAD2/parameters.py
@@ -0,0 +1,44 @@
+def parameters(dataset, lable_true):
+
+    # General Parameters
+    k = len(set(lable_true))     # number of clusters.
+    t_max = 100                  # maximum number of iterations.
+    Restarts = 10                # number of FCM restarts.
+    fuzzy_degree = 2             # fuzzy membership degree
+
+    # specific parameters
+    if dataset=='iris':
+        q = 2                     # the exponent value for the feature weight updates (β parameter in the original Paper).
+    elif dataset=='balance':
+        pass
+    elif dataset=='breast':
+        pass
+    elif dataset=='bupa':
+        pass
+    elif dataset=='cmc':
+        pass
+    elif dataset=='coil2':
+        pass
+    elif dataset=='dermatology':
+        pass
+    elif dataset=='ecoli':
+        pass
+    elif dataset=='fertility':
+        pass
+    elif dataset=='glass':
+        pass
+    elif dataset=='seed':
+        pass
+    elif dataset=='seismic':
+        pass
+    elif dataset=='zoo':
+        pass
+    elif dataset=='wine':
+        pass
+    elif dataset=='vowel':
+        pass
+    elif dataset=='thyroid':
+        pass
+    elif dataset=='soybean':
+        pass
+    return k, t_max, Restarts, fuzzy_degree, q
