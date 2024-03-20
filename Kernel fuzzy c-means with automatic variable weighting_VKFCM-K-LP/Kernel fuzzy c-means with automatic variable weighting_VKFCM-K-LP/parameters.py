c709c3cfd9b5cc6bfb827fbca714f444e97a2581
Amin Golzari Oskouei <51666357+Amin-Golzari-Oskouei@users.noreply.github.com>
2024-03-21 01:02:33 +0330

    parameters.py

--- Kernel fuzzy c-means with automatic variable weighting_VKFCM-K-LP/Kernel fuzzy c-means with automatic variable weighting_VKFCM-K-LP/parameters.py
+++ Kernel fuzzy c-means with automatic variable weighting_VKFCM-K-LP/Kernel fuzzy c-means with automatic variable weighting_VKFCM-K-LP/parameters.py
@@ -1,53 +1,53 @@
 def parameters(dataset, lable_true):
 
     # General Parameters
-    k = len(set(lable_true))     # number of clusters.
-    t_max = 100                  # maximum number of iterations.
-    Restarts = 10                # number of FCM restarts.
-    fuzzy_degree = 2             # fuzzy membership degree
+    k = len(set(lable_true))  # number of clusters.
+    t_max = 100  # maximum number of iterations.
+    Restarts = 10  # number of FCM restarts.
+    fuzzy_degree = 2  # fuzzy membership degree
 
     # specific parameters
-    if dataset=='iris':
+    if dataset == "iris":
         sigma = 1
-    elif dataset=='balance':
+    elif dataset == "balance":
         sigma = 1000000
-    elif dataset=='breast':
-        sigma = 10
-    elif dataset=='bupa':
-        sigma = 0.01
-    elif dataset=='cancer':
-        sigma = 100
-    elif dataset=='Car_evaluation':
-        sigma = 0.0000001
-    elif dataset=='dermatology':
-        sigma = 10
-    elif dataset=='diabet':
-        sigma = 100000
-    elif dataset=='ecoli':
-        sigma = 10
-    elif dataset=='glass':
-        sigma = 0.0001
-    elif dataset=='heberman':
-        sigma = 0.01
-    elif dataset=='ionosphere':
-        sigma = 10
-    elif dataset=='heart':
-        sigma = 10
-    elif dataset=='letters':
+    elif dataset == "breast":
         sigma = 1
-    elif dataset=='seed':
-        sigma = 100000
-    elif dataset=='synthetic':
-        sigma = 0.1
-    elif dataset=='spectfheart':
-        sigma = 0.1
-    elif dataset=='zoo':
-        sigma = 10
-    elif dataset=='wine':
-        sigma = 0.0001
-    elif dataset=='thyroid':
+    elif dataset == "bupa":
         sigma = 1000000
-    elif dataset=='soybean':
+    elif dataset == "cancer":
         sigma = 10
-        
+    elif dataset == "Car_evaluation":
+        sigma = 1000
+    elif dataset == "dermatology":
+        sigma = 1
+    elif dataset == "diabet":
+        sigma = 1000
+    elif dataset == "ecoli":
+        sigma = 1
+    elif dataset == "glass":
+        sigma = 1
+    elif dataset == "heberman":
+        sigma = 1000000
+    elif dataset == "ionosphere":
+        sigma = 1
+    elif dataset == "heart":
+        sigma = 10
+    elif dataset == "letters":
+        sigma = 1000000
+    elif dataset == "seed":
+        sigma = 10
+    elif dataset == "synthetic":
+        sigma = 10
+    elif dataset == "spectfheart":
+        sigma = 10
+    elif dataset == "zoo":
+        sigma = 1
+    elif dataset == "wine":
+        sigma = 1
+    elif dataset == "thyroid":
+        sigma = 1
+    elif dataset == "soybean":
+        sigma = 1
+
     return k, t_max, Restarts, fuzzy_degree, sigma
